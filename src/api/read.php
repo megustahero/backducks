b1cdcdfee7c95de75214c0d4827110047a417bf7
majorosm <matyas.majoros@dpdgroup.com>
2023-09-19 17:02:23 +0200

    big mods, not good yet

--- src/api/read.php
+++ src/api/read.php
@@ -2,39 +2,63 @@
 
     header("Access-Control-Allow-Origin: *");
     header("Content-Type: application/json; charset=UTF-8");
+    header("Access-Control-Allow-Methods: GET");
+    header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
 
     include_once '../config/database.php';
     include_once '../class/detours.php';
 
     $database = new Database();
     $db = $database->getConnection();
-    $items = new Detour($db);
-    $stmt = $items->getDetours();
-    $itemCount = $stmt->rowCount();
+    $detObj = new Detour($db);
 
-    echo json_encode($itemCount);
+    $detourTable = 'detour';
 
-    if($itemCount > 0){
-        $detourArr = array();
-        $detourArr["body"] = array();
-        $detourArr["itemCount"] = $itemCount;
-        while ($row = $stmt->fetch(PDO::FETCH_ASSOC)){
-            extract($row);
-            $e = array(
-                "id" => $id,
-                "parcel_number" => $parcel_number,
-                "type" => $type,
-                "delivery_day" => $delivery_day,
-                "insert_date" => $insert_date
-            );
-            array_push($detourArr["body"], $e);
+    $requestMethod = $_SERVER['REQUEST_METHOD'];
+
+    if ($requestMethod == 'GET') {
+        if (isset($_GET['id'])) {
+            $detId = $_GET['id'];
+            $getDetourDetails = $detObj->getLastDetour($detourTable, $detId);
+        } else {
+            $getDetourDetails = $detObj->getDetours($detourTable);
         }
-        echo json_encode($detourArr);
+        echo $getDetourDetails;
+    } else {
+        $data = [
+            'status'  => 405,
+            'message' => $requestMethod. ' Method not allowed',
+        ];
+        header("HTTP/1.0 405 Method not allowed");
+        echo json_encode($data);
     }
-    else{
-        http_response_code(404);
-        echo json_encode(
-            array("message" => "No detour found.")
-        );
-    }
+
+    #$stmt = $items->getDetours();
+    #$itemCount = $stmt->rowCount();
+
+    #echo json_encode($itemCount);
+
+    #if($itemCount > 0){
+    #    $detourArr = array();
+    #    $detourArr["body"] = array();
+    #    $detourArr["itemCount"] = $itemCount;
+    #    while ($row = $stmt->fetch(PDO::FETCH_ASSOC)){
+    #        extract($row);
+    #        $e = array(
+    #            "id" => $id,
+    #            "parcel_number" => $parcel_number,
+    #            "type" => $type,
+    #            "delivery_day" => $delivery_day,
+    #            "insert_date" => $insert_date
+    #        );
+    #        array_push($detourArr["body"], $e);
+    #    }
+    #    echo json_encode($detourArr);
+    #}
+    #else{
+    #    http_response_code(404);
+    #    echo json_encode(
+    #        array("message" => "No detour found.")
+    #    );
+    #}
 ?>
--- src/api/update.php
+++ src/api/update.php
@@ -1,8 +1,9 @@
 <?php
 
+    error_reporting(0);
     header("Access-Control-Allow-Origin: *");
     header("Content-Type: application/json; charset=UTF-8");
-    header("Access-Control-Allow-Methods: POST");
+    header("Access-Control-Allow-Methods: PUT");
     header("Access-Control-Max-Age: 3600");
     header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
 
@@ -11,24 +12,23 @@
 
     $database = new Database();
     $db = $database->getConnection();
+    $detObj = new Detour($db);
     
-    $item = new Detour($db);
-    
-    $data = json_decode(file_get_contents("php://input"));
-    
-    $item->id = $data->id;
-    
-    // Detour values
-    $item->parcel_number = $data->parcel_number;
-    $item->type = $data->type;
-    $item->delivery_day = $data->delivery_day;
-    $item->insert_date = date('Y-m-d H:i:s');
-    
-    if($item->updateDetour()){
-        echo json_encode("Detour updated.");
-    } else{
-        echo json_encode("Data could not be updated");
-    }
+    $detourTable = 'detour';
 
+    $requestMethod = $_SERVER['REQUEST_METHOD'];
+
+    if ($requestMethod == 'PUT') {
+        $formData = json_decode(file_get_contents("php://input"), true);
+        $updateDetourRecord = $detObj->updateDetour($detourTable, $formData, $_GET);
+        echo $updateDetourRecord;
+    } else {
+        $data = [
+            'status'  => 405,
+            'message' => $requestMethod . ' Method not allowed',
+        ];
+        header("HTTP/1.0 405 Method not allowed");
+        echo json_encode($data);
+    }
 
 ?>
